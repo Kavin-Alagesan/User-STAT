538f30e1d120a175f3c44e018f823561ad8994a6
Kavin-Alagesan <96373698+Kavin-Alagesan@users.noreply.github.com>
2021-12-27 19:23:16 +0530

    updated for deployment

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 import yaml
+#import json, time
+from datetime import date
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -17,6 +19,7 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    current_date = str(date.today())
     ip_add = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     cur = mysql.connection.cursor()
     resultvalue = cur.execute('SELECT * FROM ip WHERE ip_address = %s', [ip_add])
@@ -25,18 +28,29 @@ def index():
         address = get_ip_add['ip_address']
         count = get_ip_add['visit_count'] + 1
         cur = mysql.connection.cursor()
-        cur.execute("UPDATE ip SET visit_count = %s WHERE ip_address = %s", (count, address))
+        cur.execute("INSERT INTO ip(ip_address, visit_count, date) VALUES (%s, %s, %s)", (address, count, current_date))
         mysql.connection.commit()
         cur.close()
-        print('You have visited ' + str(count) + ' times to this page')
+        print('Date: ' + str(current_date) +
+              'ip: ' + address +
+              'count: ' + str(count))
     else:
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO ip (ip_address, visit_count) VALUES (%s, %s)', (ip_add, 1))
+        cur.execute('INSERT INTO ip(ip_address, visit_count, date) VALUES (%s, %s)', (ip_add, 1, current_date))
         mysql.connection.commit()
         cur.close()
-        print('You visited first time to this page')
+        print('Date: ' + str(current_date) +
+              'ip: ' + ip_add +
+              'count: ' + 1)
 
     return render_template('base.html')
+
+#@app.route('/time/')
+#def time():
+#    data_set = {'Visiter ID': user_id, 'No of visits' : visit_count, 'Date' : time.time() }
+#    json_dump = json.dumps(data_set)
+
+#    return json_dump
 
 if __name__ == '__main__':
     app.run(debug=True)
